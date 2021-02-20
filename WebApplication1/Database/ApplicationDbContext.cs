eca466bbdb7061929ec7eec03441d34c267a3d1b
Pedro Pimenta <pedropimenta@rocketmail.com>
2021-02-20 10:49:34 -0300

    First commit

---
+++ WebApplication1/Database/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+        public DbSet<WebApplication1.Models.Product> Product { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Need product's name")]
+        public string Name { get; set; }
+        [Range(1, 1000, ErrorMessage ="Value out of range")]
+        public int Quantity { get; set; }
+        
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
